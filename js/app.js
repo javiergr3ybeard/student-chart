5b31a1434a97d44ea4cd387e36bf788555dde1df
Jordan Tompkins <jtompkins28@gmail.com>
2016-06-02 11:27:05 -0400

    made a delete button still need to make a delete function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $(document).foundation()
 
+function createButton(context, func){
+  var button = document.createElement("input");
+  button.type = "button";
+  button.value = "Delete";
+  button.onclick = func;
+  context.appendChild(button);
+}
+
+window.onload = function(){
+    createButton(document.body,function(){
+      highlight(this.parentNode.childNodes[1]);
+    });
+}
+
 
 var myForm = document.querySelector('form');
 myForm.onsubmit = function(ev) {
@@ -8,10 +22,10 @@ myForm.onsubmit = function(ev) {
 
   var studentName = this.studentName.value;
 
-
   details.innerHTML += ' \
     <dl> \
       <dt>Student Name</dt> \
       <dd>' + studentName + '</dd> \
     </dl>';
+
 };
